795d09df99baa5c9e6995e248cf31000825c5a34
sebastien.metay@gmail.com <sebastien.metay@gmail.com>
2013-02-08 09:37:42 +0100

    add pom + src android

---
+++ TazPadApp/src/com/taztag/tazpad/KevoreeAndroid/src/main/java/IHM_Tazpad.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sebastien
+ * Date: 08/02/13
+ * Time: 09:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class IHM_Tazpad {
+}
